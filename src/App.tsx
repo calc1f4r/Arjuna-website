21269e32a26d683e6ee3e1070dbedec0b3a71167
calc1f4r <20yashs09@gmail.com>
2025-04-28 09:45:04 +0000

    fix: remove NotFound route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,6 @@ const App = () => (
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/code-examples" element={<BlogPostWithCode />} />
             <Route path="/blog/:id" element={<BlogPost />} />
-            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </BrowserRouter>
